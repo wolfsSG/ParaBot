7542a091e66771ecd3e391dd107f1f5818e7043f
wolfsSG <worwol@rambler.ru>
2025-05-09 23:47:55 +0700

    Update reminder.sh

--- para_kzn_bot/reminder.sh
+++ para_kzn_bot/reminder.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-source venv/bin/activate
-cd /usr/local/www/www9/para_kzn_bot/bot
-python /usr/local/www/www9/para_kzn_bot/bot/reminder.py
+# Пути
+VENV_PATH="/home/user/parabot_venv/bin/activate"
+PROJECT_DIR="/home/user/ParaBot/para_kzn_bot/bot"
+
+# Активируем окружение
+source "$VENV_PATH" || {
+    exit 1
+}
+
+# Ждем инициализации
+sleep 5
+
+# Переходим и запускаем
+cd "$PROJECT_DIR" || {
+    exit 1
+}
+
+python reminder.py
+
+exit $?
